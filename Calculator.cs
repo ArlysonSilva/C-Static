658e78d48a71cee6a1bd9a81dac183bfa26ee394
arlyson <arlyson2000@hotmail.com>
2025-05-25 14:51:48 -0300

    Finished

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+class Calculator
+{
+    // atribute numeric
+    public static int number = 10;
+
+    //method to sum two values
+    public static void Sum(int n1, int n2)
+    {
+        Console.WriteLine($"the sum of {n1} + {n2} is = {n1 + n2}");
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//displaying value conditional on number attribute
+Console.WriteLine($"The value is {Calculator.number}");
+
+//Sum 
+
+Calculator.Sum(8, 10);
